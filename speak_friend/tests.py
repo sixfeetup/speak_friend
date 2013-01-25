a8fa38ac373ed08b92caef2fcdbf73d9ea35fcfd
Nolan Brubaker <nolan@sixfeetup.com>
2013-01-25 18:08:07 -0500

    Some rudimentary tests.

--- speak_friend/tests.py
+++ speak_friend/tests.py
@@ -1,17 +1,36 @@
-import unittest
+from unittest import TestCase
 
 from pyramid import testing
 
+from .api import TemplateAPI
+from .subscribers import register_api
+from .views.accounts import create_account
 
-class ViewTests(unittest.TestCase):
+
+class SFBaseCase(TestCase):
     def setUp(self):
         self.config = testing.setUp()
+        self.config.add_settings({'site_name': "Test"})
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
+
+class ViewTests(SFBaseCase):
+    def test_create_account_view(self):
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], '.')
+        info = create_account(request)
+        self.assertEqual(len(info.keys()), 0)
+
+
+class SubscriberTests(SFBaseCase):
+    def test_register_api(self):
+        event_dict = {}
+        register_api(event_dict)
+        self.assertTrue('api' in event_dict.keys())
+
+
+class TemplateAPITests(SFBaseCase):
+    def test_settings(self):
+        api = TemplateAPI()
+        self.assertTrue(hasattr(api, 'settings'))
