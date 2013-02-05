d2ea50bdfe0a208f35adc1eccbabd809311af71d
Nolan Brubaker <nolan@sixfeetup.com>
2013-02-05 12:02:59 -0500

    Add necessary model methods. Refs #1489

--- speak_friend/models/openid.py
+++ speak_friend/models/openid.py
@@ -5,6 +5,7 @@ from speak_friend.models import Base
 from zope.interface import implements
 
 from speak_friend.interfaces import IOpenIDStore
+from speak_friend.models import DBSession
 
 
 class Association(Base):
@@ -17,6 +18,17 @@ class Association(Base):
     lifetime = Column(Integer)
     assoc_type = Column(UnicodeText)
 
+    def __init__(self, server_url, handle, secret,
+                 issued, lifetime, assoc_type):
+        self.server_url = server_url
+        self.handle = handle
+        self.secret = secret
+        self.issued = issued
+        self.lifetime = lifetime
+        self.assoc_type = assoc_type
+
+    def __repr__(self):
+        return u"<Association(%s, %s)>" % (self.server_url, self.handle)
 
 class Nonce(Base):
     __tablename__ = 'nonces'
@@ -26,15 +38,28 @@ class Nonce(Base):
     timestamp = Column(Integer, primary_key=True)
     salt = Column(UnicodeText, primary_key=True)
 
+    def __init__(self, server_url, timestamp, salt):
+        self.server_url = server_url
+        self.timestamp = timestamp
+        self.salt = salt
+
+    def __repr__(self):
+        return u"<Nonce(%s)>" % self.server_url
+
 
 class SFOpenIDStore(object):
     implements(IOpenIDStore)
 
+    def __init__(self, session):
+        self.session = session
+
     def storeAssociation(self, server_url, association):
-        pass
+        self.session.add(association)
+        self.session.commit()
 
     def getAssociation(self, server_url, handle=None):
-        pass
+        association = self.session.query.filter_by(server_url=server_url).first()
+        return association
 
     def removeAssociation(self, server_url, handle):
         pass
