4071290d2faccd072c67e024f18504832393bffd
cewing <cris@crisewing.com>
2013-02-15 14:53:18 -0800

    * add initialization and representation, fix up table args.

--- speak_friend/models/profiles.py
+++ speak_friend/models/profiles.py
@@ -9,9 +9,20 @@ from speak_freind.models import Base
 
 
 class Domain(Base):
-    __tablename__ = "domains"
-    __table_args__ = {'schema': 'profiles'}
+    __tablename__ = 'domain_profiles'
+    __table_args__ = (
+        {'schema': 'profiles'}
+    )
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, length=128)
-    password_valid = Column(Integer) # minutes, seconds, hours, days??
+    password_valid = Column(Integer) # minutes
     max_attempts = Column(SmallInteger)
+
+    def __init__(self, id, name, password_valid, max_attempts):
+        self.id = id
+        self.name = name
+        self.password_valid = password_valid
+        self.max_attempts = max_attempts
+
+    def __repr__(self):
+        return u'<DomainProfile(%s)>' % self.name
