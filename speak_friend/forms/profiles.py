b43fb6fae87f128b26814d20594bbeb4a6df93c3
cewing <cris@crisewing.com>
2013-02-18 18:07:23 -0800

    * add FQDN validator to verify that entered domain names are in fact fqdns (see http://stackoverflow.com/questions/4912520/validate-fqdn-in-c-sharp for referene) * set up colander schema for the domain_profile model. * negative values will be allowed for password_valid and max_attempts.  The presence of a negative value in either field indicates that a system default should be fetched and used in the moment.
    refs #1478

--- speak_friend/forms/profiles.py
+++ speak_friend/forms/profiles.py
@@ -1,4 +1,7 @@
-from colander import Bool, Email, MappingSchema, SchemaNode, String, Integer
+import re
+
+from colander import Bool, MappingSchema, SchemaNode, String, Integer
+from colander import Email, Regex
 from deform import Form
 from deform.widget import CheckedInputWidget, CheckedPasswordWidget
 
@@ -20,10 +23,37 @@ class Profile(MappingSchema):
 profile_form = Form(Profile(), buttons=('submit', 'cancel'))
 
 
+fqdn_re = re.compile(
+    r'(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)')
+
+
+class FQDN(Regex):
+    """Validator for a Fully Qualified Domain Name
+
+    If ``msg`` is supplied, it will be the error message to be used when 
+    raising `colander.Invalid`; otherwise, defaults to 'Invalid domain name'
+    """
+    def __init__(self, msg=None):
+        if msg is None:
+            msg = "Invalid domain name"
+        super(FQDN, self).__init__(fqdn_re, msg=msg)
+
+
 class Domain(MappingSchema):
-    name = SchemaNode(String())
-    password_valid = SchemaNode(Integer())
-    max_attempts = SchemaNode(Integer())
-
-
-domain_form = Form(Domain(), buttons=('submit', 'cancel'))
+    name = SchemaNode(
+        String(),
+        title="Domain Name",
+        description="Must be a valid Fully Qualified Domain Name",
+        validator=FQDN())
+    password_valid = SchemaNode(
+        Integer(),
+        title="Password valid",
+        description="Indicate the length of time, in minutes that a password "
+                    "should be valid (a negative value will use the system "
+                    "default)")
+    max_attempts = SchemaNode(
+        Integer(),
+        title="Maximum login attempts",
+        description="Indicate the number of times a user may fail a login "
+                    "attempt before being disabled (a negative value will "
+                    "use the system default)")
