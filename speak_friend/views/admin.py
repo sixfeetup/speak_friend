0fbd10f0b79aff9a41eb64e9f968b494708622ec
cewing <cris@crisewing.com>
2013-02-18 18:11:34 -0800

    * update domain editing view  * process submitted data, and create a new object if it is a new one, or edit an existing if there is already one by that name.
    refs #1478

--- speak_friend/views/admin.py
+++ speak_friend/views/admin.py
@@ -1,8 +1,29 @@
 # Views related to administrator actions. (deactivating accounts,
 # changing user passwords)
-from speak_friend.forms.profiles import domain_form
+from deform import Form
+from deform import ValidationFailure
+
+from speak_friend.models import DBSession
+from speak_friend.forms.profiles import Domain
+from speak_friend.models.profiles import DomainProfile
 
 
 def create_domain(request):
-    form = domain_form.render()
-    return {'form': form}
+    domain_schema = Domain()
+    domain_form = Form(domain_schema, buttons=('submit', 'cancel', ))
+    if 'submit' in request.POST:
+        # the form has been submitted
+        controls = request.POST.items()
+        try:
+            appstruct = domain_form.validate(controls)
+        except ValidationFailure, e:
+            # form failed validation
+            return {'form': e.render()}
+        # form valid outcome
+        session = DBSession()
+        new_domain = DomainProfile(**appstruct)
+        session.merge(new_domain)
+        return {'form': domain_form.render(appstruct)}
+
+    # form not yet submitted outcome
+    return {'form': domain_form.render()}
