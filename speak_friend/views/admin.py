a0fdbb0ae8ce3bbdb7c79c6a4aaec4ef2fa90079
cewing <cris@crisewing.com>
2013-02-25 15:21:55 -0800

    remove custom slots from the base template replace with a css repeater based on resources passed in from the view update the create-domain view to use this pattern.

--- speak_friend/views/admin.py
+++ speak_friend/views/admin.py
@@ -11,6 +11,8 @@ from speak_friend.models.profiles import DomainProfile
 def create_domain(request):
     domain_schema = Domain()
     domain_form = Form(domain_schema, buttons=('submit', 'cancel', ))
+    js_resources = ['deform:static/scripts/deform.js',]
+    css_resources = ['deform:static/css/form.css',]
     if 'submit' in request.POST:
         # the form has been submitted
         controls = request.POST.items()
@@ -18,12 +20,18 @@ def create_domain(request):
             appstruct = domain_form.validate(controls)
         except ValidationFailure, e:
             # form failed validation
-            return {'form': e.render()}
+            return {'form': e.render(),
+                    'js_resources': js_resources,
+                    'css_resources': css_resources}
         # form valid outcome
         session = DBSession()
         new_domain = DomainProfile(**appstruct)
         session.merge(new_domain)
-        return {'form': domain_form.render(appstruct)}
+        return {'form': domain_form.render(appstruct),
+                'js_resources': js_resources,
+                'css_resources': css_resources}
 
     # form not yet submitted outcome
-    return {'form': domain_form.render()}
+    return {'form': domain_form.render(),
+            'js_resources': js_resources,
+            'css_resources': css_resources}
