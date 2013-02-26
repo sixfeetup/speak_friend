b91503dccb6b4d5b2c582557a7ef7f093451a54b
David Blewett <davidb@sixfeetup.com>
2013-02-26 11:17:14 -0500

    Initial static asset override implementation: * Add blank `custom.css` file that can be overridden * Add macros in individual files   * footer   * quick_links Conflicts:
    	speak_friend/templates/base.pt

--- speak_friend/api.py
+++ speak_friend/api.py
@@ -1,6 +1,7 @@
 # The API that should be available to templates.
 
 
+from pyramid.renderers import get_renderer
 from pyramid.threadlocal import get_current_registry
 
 
@@ -11,3 +12,12 @@ class TemplateAPI(object):
     @property
     def settings(self):
         return get_current_registry().settings
+
+    @property
+    def macros(self):
+        macro_names = ['footer', 'quick_links']
+        all_macros = {}
+        for mname in macro_names:
+            renderer = get_renderer('templates/macros/%s#%s.pt' % (mname, mname))
+            all_macros[mname] = renderer.implementation()
+        return all_macros
