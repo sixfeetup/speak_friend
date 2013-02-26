c7d8728482223b06db170b00dd357998dd3dd344
David Blewett <davidb@sixfeetup.com>
2013-02-26 15:36:31 -0500

    Refactor `TemplateAPI` handling of form resources: * If a `forms` variable is present in the render values,   use `get_widget_resources` to automatically generate all required   CSS/JS paths * Update `base.pt` to new convention * Update views to new convention * Remove other usages of `get_widget_resources`

--- speak_friend/views/accounts.py
+++ speak_friend/views/accounts.py
@@ -4,9 +4,15 @@ from speak_friend.forms.profiles import profile_form
 
 def create_profile(request):
     form = profile_form.render()
-    return {'form': form}
+    return {
+        'forms': [form],
+        'rendered_form': form,
+    }
 
 
 def edit_profile(request):
     form = profile_form.render()
-    return {'form': form}
+    return {
+        'forms': [form],
+        'rendered_form': form,
+    }
