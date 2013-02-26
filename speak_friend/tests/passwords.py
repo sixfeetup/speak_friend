6b78d4ad81ad795b018a0f5bb5f30c5223324d2d
cewing <cris@crisewing.com>
2013-02-26 15:22:49 -0800

    refactoring tests, 50% finished.
    refs #1512

--- speak_friend/tests/passwords.py
+++ speak_friend/tests/passwords.py
@@ -4,13 +4,14 @@ import ConfigParser
 from unittest import TestCase
 
 
-from speak_friend.passwords import check_password
-from speak_friend.passwords import make_disallowed_regex
-from speak_friend.passwords import get_chartype_counts
-from speak_friend.passwords import password_settings
-from speak_friend.passwords import DEFAULT_PASSWORD_SETTINGS
+# from speak_friend.passwords import check_password
+# from speak_friend.passwords import make_disallowed_regex
+# from speak_friend.passwords import get_chartype_counts
+# from speak_friend.passwords import password_settings
+# from speak_friend.passwords import DEFAULT_PASSWORD_SETTINGS
 from speak_friend.passwords import PASSWORD_SETTINGS_PREFIX
-from speak_friend.passwords import ERROR_MESSAGES
+from speak_friend.passwords import PasswordValidator
+# from speak_friend.passwords import ERROR_MESSAGES
 from speak_friend.tests.common import SFBaseCase
 
 
@@ -22,7 +23,8 @@ PASSWORD_CONFIG.read(INI_FILE_PATH)
 
 class PasswordToolsTests(TestCase):
     def setUp(self):
-        self.settings = DEFAULT_PASSWORD_SETTINGS.copy()
+        # self.settings = DEFAULT_PASSWORD_SETTINGS.copy()
+        self.validator = PasswordValidator()
         self.passwords = {
             'alpha_lower': ['abcdef', [6,0,0,0]],
             'alpha_upper': ['ABCDEF', [0,6,0,0]],
@@ -42,7 +44,7 @@ class PasswordToolsTests(TestCase):
         will always return the count as lower, upper, numeric, special
         """
         out = []
-        counts = get_chartype_counts(password)
+        counts = self.validator._get_chartype_counts(password)
         for key in ['min_lower', 'min_upper', 'min_numeric', 'min_special']:
             out.append(counts[key])
         return out
@@ -62,7 +64,7 @@ class PasswordToolsTests(TestCase):
         for disallowed, keymatch in [('abcABC', 'alpha'),
                                      ('123', 'numeric'),
                                      ('$+]', 'complex')]:
-            regex = make_disallowed_regex(disallowed)
+            regex = self.validator._make_disallowed_regex(disallowed)
             for key, val in self.passwords.items():
                 password = val[0]
                 found = bool(len(regex.findall(password)))
@@ -75,14 +77,16 @@ class PasswordToolsTests(TestCase):
         """default settings allow any password to pass"""
         for key, val in self.passwords.items():
             password = val[0]
-            self.assertEqual(check_password(password, self.settings), None)
+            self.assertEqual(self.validator(password), None)
 
     def test_password_min_length(self):
-        self.settings['min_length'] = 7
-        expected_error = ERROR_MESSAGES['min_length'] % self.settings
+        testkey = 'min_length'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: 7})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if 'long' in key:
                 self.assertEqual(result, None)
             else:
@@ -90,23 +94,27 @@ class PasswordToolsTests(TestCase):
                 self.assertEqual(result, expected_error)
 
     def test_password_max_length(self):
-        self.settings['max_length'] = 10
-        expected_error = ERROR_MESSAGES['max_length'] % self.settings
+        testkey = 'max_length'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: 7})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
-            if 'long' in key:
+            result = validator(password)
+            if 'long' not in key:
+                self.assertEqual(result, None)
+            else:
                 self.assertTrue(result is not None)
                 self.assertEqual(result, expected_error)
-            else:
-                self.assertEqual(result, None)
 
     def test_password_min_lower(self):
-        self.settings['min_lower'] = 5
-        expected_error = ERROR_MESSAGES['min_lower'] % self.settings
+        testkey = 'min_lower'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: 5})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if 'alpha_lower' in key or 'long_complex' in key:
                 self.assertEqual(result, None)
             else:
@@ -114,11 +122,13 @@ class PasswordToolsTests(TestCase):
                 self.assertEqual(result, expected_error)
 
     def test_password_min_upper(self):
-        self.settings['min_upper'] = 5
-        expected_error = ERROR_MESSAGES['min_upper'] % self.settings
+        testkey = 'min_upper'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: 5})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if 'alpha_upper' in key or 'long_complex' in key:
                 self.assertEqual(result, None)
             else:
@@ -126,11 +136,13 @@ class PasswordToolsTests(TestCase):
                 self.assertEqual(result, expected_error)
 
     def test_password_min_numeric(self):
-        self.settings['min_numeric'] = 2
-        expected_error = ERROR_MESSAGES['min_numeric'] % self.settings
+        testkey = 'min_numeric'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: 2})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if 'numeric' in key or 'long_complex' in key:
                 self.assertEqual(result, None)
             else:
@@ -138,11 +150,13 @@ class PasswordToolsTests(TestCase):
                 self.assertEqual(result, expected_error)
 
     def test_password_min_special(self):
-        self.settings['min_special'] = 3
-        expected_error = ERROR_MESSAGES['min_special'] % self.settings
+        testkey = 'min_special'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: 3})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if key in ['special', 'long_complex']:
                 self.assertEqual(result, None)
             else:
@@ -150,11 +164,13 @@ class PasswordToolsTests(TestCase):
                 self.assertEqual(result, expected_error)
 
     def test_password_disallowed(self):
-        self.settings['disallowed'] = '$+]'
-        expected_error = ERROR_MESSAGES['disallowed'] % self.settings
+        testkey = 'disallowed'
+        key = '.'.join([PASSWORD_SETTINGS_PREFIX, testkey])
+        validator = PasswordValidator({key: '$+]'})
+        expected_error = validator._get_error_message(testkey)
         for key, val in self.passwords.items():
             password = val[0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if 'complex' not in key:
                 self.assertEqual(result, None)
             else:
@@ -163,79 +179,81 @@ class PasswordToolsTests(TestCase):
 
     def test_password_multiple_errors(self):
         keys = ['min_lower', 'min_upper', 'min_numeric', 'min_special']
+        keys = map(lambda x: '.'.join([PASSWORD_SETTINGS_PREFIX, x]), keys)
         expected_error_keys = ['min_numeric', 'min_special', 'min_length']
-        for key in keys:
-            self.settings[key] = 2
-        self.settings['min_length'] = 8
+        settings = dict([(key, 2) for key in keys])
+        settings['.'.join([PASSWORD_SETTINGS_PREFIX, 'min_length'])] = 8
+        validator = PasswordValidator(settings)
         for pwkey in ['complex', 'long_complex']:
             password = self.passwords[pwkey][0]
-            result = check_password(password, self.settings)
+            result = validator(password)
             if 'long' in pwkey:
                 self.assertEqual(result, None)
             else:
-                self.assertTrue(result is not None)
+                msg = "%s passed but should not have"
+                self.assertTrue(result is not None, msg % pwkey)
                 for errkey in expected_error_keys:
-                    errmsg = ERROR_MESSAGES[errkey] % self.settings
-                    self.assertTrue(errmsg in result)
-
-
-class PasswordSettingsTests(SFBaseCase):
-    """verify that integratin with actual settings works correctly"""
-    def setUp(self):
-        super(PasswordSettingsTests, self).setUp()
-        self.prefix = PASSWORD_SETTINGS_PREFIX
-        self.use_settings = {
-            'min_length': (10, int),
-            'max_length': (10, int),
-            'min_lower': (10, int),
-            'min_upper': (10, int),
-            'min_numeric': (10, int),
-            'min_special': (10, int),
-            'disallowed': ("$#!%&{'[\"}])(", str)
-        }
-
-    def test_set_integer_vals(self):
-        for key in ['min_length', 'max_length', 'min_lower', 'min_upper',
-                    'min_numeric', 'min_special']:
-            regkey = '.'.join([self.prefix, key])
-            expected, expected_type = self.use_settings[key]
-            self.config.add_settings(
-                dict(PASSWORD_CONFIG.items(key)))
-            settings = password_settings()
-            actual = settings[key]
-            self.assertEqual(expected, actual)
-            self.assertTrue(isinstance(actual, expected_type))
-
-    def test_set_string_val(self):
-        key = 'disallowed'
-        expected, expected_type = self.use_settings[key]
-        self.config.add_settings(
-            dict(PASSWORD_CONFIG.items(key)))
-        settings = password_settings()
-        actual = settings[key]
-        self.assertEqual(expected, actual)
-        self.assertTrue(isinstance(actual, expected_type))
-
-    def test_set_all_vals(self):
-        confkey = 'multiple'
-        self.config.add_settings(
-            dict(PASSWORD_CONFIG.items(confkey)))
-        for key in DEFAULT_PASSWORD_SETTINGS.keys():
-            expected, expected_type = self.use_settings[key]
-            settings = password_settings()
-            actual = settings[key]
-            self.assertEqual(expected, actual)
-            self.assertTrue(isinstance(actual, expected_type))
-
-    def test_set_nonevals(self):
-        """what happens when the value in .ini is nothing
-        """
-        confkey = 'nonevals'
-        self.config.add_settings(
-            dict(PASSWORD_CONFIG.items(confkey)))
-        for key in DEFAULT_PASSWORD_SETTINGS.keys():
-            expected_type = self.use_settings[key][1]
-            expected = DEFAULT_PASSWORD_SETTINGS[key]
-            settings = password_settings()
-            actual = settings[key]
-            self.assertEqual(expected, actual, 'bad value for %s' % key)
+                    errmsg = validator._get_error_message(errkey)
+                    self.assertTrue(errmsg in result, 'missing "%s"' % errmsg)
+# 
+# 
+# class PasswordSettingsTests(SFBaseCase):
+#     """verify that integrating with actual settings works correctly"""
+#     def setUp(self):
+#         super(PasswordSettingsTests, self).setUp()
+#         self.prefix = PASSWORD_SETTINGS_PREFIX
+#         self.use_settings = {
+#             'min_length': (10, int),
+#             'max_length': (10, int),
+#             'min_lower': (10, int),
+#             'min_upper': (10, int),
+#             'min_numeric': (10, int),
+#             'min_special': (10, int),
+#             'disallowed': ("$#!%&{'[\"}])(", str)
+#         }
+# 
+#     def test_set_integer_vals(self):
+#         for key in ['min_length', 'max_length', 'min_lower', 'min_upper',
+#                     'min_numeric', 'min_special']:
+#             regkey = '.'.join([self.prefix, key])
+#             expected, expected_type = self.use_settings[key]
+#             self.config.add_settings(
+#                 dict(PASSWORD_CONFIG.items(key)))
+#             settings = password_settings()
+#             actual = settings[key]
+#             self.assertEqual(expected, actual)
+#             self.assertTrue(isinstance(actual, expected_type))
+# 
+#     def test_set_string_val(self):
+#         key = 'disallowed'
+#         expected, expected_type = self.use_settings[key]
+#         self.config.add_settings(
+#             dict(PASSWORD_CONFIG.items(key)))
+#         settings = password_settings()
+#         actual = settings[key]
+#         self.assertEqual(expected, actual)
+#         self.assertTrue(isinstance(actual, expected_type))
+# 
+#     def test_set_all_vals(self):
+#         confkey = 'multiple'
+#         self.config.add_settings(
+#             dict(PASSWORD_CONFIG.items(confkey)))
+#         for key in DEFAULT_PASSWORD_SETTINGS.keys():
+#             expected, expected_type = self.use_settings[key]
+#             settings = password_settings()
+#             actual = settings[key]
+#             self.assertEqual(expected, actual)
+#             self.assertTrue(isinstance(actual, expected_type))
+# 
+#     def test_set_nonevals(self):
+#         """what happens when the value in .ini is nothing
+#         """
+#         confkey = 'nonevals'
+#         self.config.add_settings(
+#             dict(PASSWORD_CONFIG.items(confkey)))
+#         for key in DEFAULT_PASSWORD_SETTINGS.keys():
+#             expected_type = self.use_settings[key][1]
+#             expected = DEFAULT_PASSWORD_SETTINGS[key]
+#             settings = password_settings()
+#             actual = settings[key]
+#             self.assertEqual(expected, actual, 'bad value for %s' % key)
