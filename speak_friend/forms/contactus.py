2982fa41272b45043fec1ec83dc17e5a9b941421
David Blewett <davidb@sixfeetup.com>
2013-02-26 12:51:37 -0500

    Update `ContactUs` schema definition to match wireframe.

--- speak_friend/forms/contactus.py
+++ speak_friend/forms/contactus.py
@@ -1,15 +1,28 @@
 from colander import Email, MappingSchema, SchemaNode, String
 from deform import Form
-from deform.widget import CheckedInputWidget
+from deform.widget import TextAreaWidget
 
 
 class ContactUs(MappingSchema):
-    contact_name = SchemaNode(String())
+    contact_name = SchemaNode(
+        String(),
+        title='Name',
+    )
     reply_email = SchemaNode(
         String(),
+        title='Email',
         validator=Email(),
-        widget=CheckedInputWidget(),
     )
+    message_body = SchemaNode(
+        String(),
+        title='Message',
+        widget=TextAreaWidget(cols=50, rows=10),
+    )
+    title='Contact Us',
+    description='Have a question or want to tell us something? Let us know.',
 
 
-contact_us_form = Form(ContactUs(), buttons=('submit', 'cancel'))
+contact_us_form = Form(
+    ContactUs(),
+    buttons=('submit',),
+)
