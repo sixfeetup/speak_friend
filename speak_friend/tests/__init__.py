0f3c3ecfe4a53986c6a4722d9dee5cdc887a11bd
cewing <cris@crisewing.com>
2013-02-26 13:48:01 -0800

    apparently I forgot to save before I commited that change.  Retrying

--- speak_friend/tests/__init__.py
+++ speak_friend/tests/__init__.py
@@ -3,8 +3,5 @@ from .templates import *
 from .subscribers import *
 from .views import *
 from .forms import *
-<<<<<<< HEAD
 from .passwords import *
-=======
 from .password_management import *
->>>>>>> master
