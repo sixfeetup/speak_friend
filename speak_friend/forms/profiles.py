6fca7704fbb7ec3dfd182681b9cc8b98523a9fe9
cewing <cris@crisewing.com>
2013-03-01 17:32:04 -0800

    * create a custom widget for the strength-calculation password input. * register custom resources and a special renderer for forms using the password widget. * use the custom widget for the profile form, and instantiate the form with the custom renderer and resource_registry args.
    refs #1466

--- speak_friend/forms/profiles.py
+++ speak_friend/forms/profiles.py
@@ -1,12 +1,39 @@
 import re
+from pkg_resources import resource_filename
 
 from colander import Bool, MappingSchema, SchemaNode, String, Integer, Invalid
 from colander import Email, Regex
 from deform import Button, Form
-from deform.widget import CheckedInputWidget, CheckedPasswordWidget
+from deform import ZPTRendererFactory
+from deform.widget import CheckedInputWidget
+from deform.widget import CheckedPasswordWidget
+from deform.widget import ResourceRegistry
 
 from speak_friend.models import DBSession
 from speak_friend.models.profiles import UserProfile
+
+
+# set a resource registry that contains resources for the password widget
+password_registry = ResourceRegistry()
+password_registry.set_js_resources('password', None,
+                                   'js/zxcvbn-async.js',
+                                   'js/password_strength.js')
+password_registry.set_css_resources('password', None,
+                                    'css/password_strength.css')
+
+
+# set a template renderer that loads both deform and speak_friend templates
+deform_path = resource_filename('deform', 'templates')
+deform_bootstrap_path = resource_filename('deform_bootstrap', 'templates')
+speak_friend_path = resource_filename('speak_friend', 'templates')
+search_path = (speak_friend_path, deform_bootstrap_path, deform_path)
+renderer = ZPTRendererFactory(search_path)
+
+
+class StrengthValidatingPasswordWidget(CheckedPasswordWidget):
+    requirements = (('jquery.maskedinput', None),
+                    ('password', None), )
+    template = 'widgets/strength_validating_password'
 
 
 class Profile(MappingSchema):
@@ -17,13 +44,17 @@ class Profile(MappingSchema):
                        validator=Email(),
                        widget=CheckedInputWidget())
     password = SchemaNode(String(),
-                          widget=CheckedPasswordWidget())
+                          widget=StrengthValidatingPasswordWidget())
     agree_to_policy = SchemaNode(Bool(),
                                  title='I agree to the usage policy.')
     captcha = SchemaNode(String())
 
 
-profile_form = Form(Profile(), buttons=('submit', 'cancel'))
+# instantiate our form with custom registry and renderer to get extra
+# templates and resources
+profile_form = Form(Profile(), buttons=('submit', 'cancel'),
+                    resource_registry=password_registry,
+                    renderer=renderer)
 
 
 fqdn_re = re.compile(
