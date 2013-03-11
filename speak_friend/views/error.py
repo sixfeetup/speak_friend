d64a70383557f1e8102108fcd2da5c5063bc3dc9
David Blewett <davidb@sixfeetup.com>
2013-03-11 14:23:07 -0400

    Make forbidden view more defensive by checking for response type.

--- speak_friend/views/error.py
+++ speak_friend/views/error.py
@@ -1,4 +1,5 @@
 from pyramid.renderers import render_to_response
+from pyramid.response import Response
 from pyramid.security import authenticated_userid
 
 from speak_friend.views.accounts import LoginView
@@ -16,11 +17,15 @@ def notallowed(request):
         if request.method == 'POST':
             # Process the login form and redirect
             view_method = login.post
-            return login.post()
         else:
             # User is not logged in, render the login form
+            view_method = login.get
+        response = view_method()
+        if isinstance(response, Response):
+            return response
+        else:
             return render_to_response("speak_friend:templates/login.pt",
-                                      login.get(),
+                                      response,
                                       request=request)
     else:
         # If the user is logged in, they don't have permission
