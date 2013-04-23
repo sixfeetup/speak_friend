6f73c4230d8677b413e47d3bd46c220608c17599
Nolan Brubaker <nolan@sixfeetup.com>
2013-04-23 14:13:46 -0400

    Remove CSRF tokens from URLs. Refs #1846
    Keeping a CSRF token in a GET URL doesn't invalidate it, which leads to
    issues when an admin edits their profile and gets redirectd back to the
    search view.

--- speak_friend/utils.py
+++ speak_friend/utils.py
@@ -18,3 +18,13 @@ def get_domain(request):
     if not referrer:
         return ''
     return urlsplit(referrer).netloc.split(':')[0]
+
+
+def remove_url_csrf(url):
+    """Remove the CSRF token from a URL with GET parameters.
+    """
+    token_ident = '&csrf_token='
+    start_index = url.find(token_ident)
+    end_index = start_index + 40 + len(token_ident)
+    url = url[:start_index] + url[end_index:]
+    return url
