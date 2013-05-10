b994bc24b7f8a6ad14f7ce43813ba0f8520dd976
David Blewett <davidb@sixfeetup.com>
2013-05-10 02:44:52 -0400

    This test moved to `sixfeetup.bowab`.

--- speak_friend/tests/__init__.py
+++ speak_friend/tests/__init__.py
@@ -2,7 +2,6 @@ from .interfaces import *
 from .forms import *
 from .models.open_id import *
 from .passwords import *
-from .subscribers import *
 from .templates import *
 from .utils import *
 from .views import *
