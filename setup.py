f16b3870d8228d804dd7b4a1acd2aac2f4f73758
Chrissy Wainwright <cwainwright@gmail.com>
2013-11-07 13:11:34 -0500

    Prepare speak_friend 0.53.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = '0.52'
+version = '0.53'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
