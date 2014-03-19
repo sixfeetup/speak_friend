f581bcc7edebcebc8f8d5f33e7c71a7d2dc08cf5
Chrissy Wainwright <cwainwright@gmail.com>
2014-03-19 14:10:04 -0400

    bumping version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = '0.60'
+version = '0.61'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
