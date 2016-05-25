e49ae171fc90bb3820ff1436b8364fb09dca58cb
Rob McBroom <rob@sixfeetup.com>
2016-05-25 13:50:59 -0400

    ignore return_to in GET if it was passed to the function

--- speak_friend/views/accounts.py
+++ speak_friend/views/accounts.py
@@ -758,8 +758,8 @@ class LoginView(object):
             return HTTPFound(location=url, headers=headers)
 
 
-def logout(request, return_to = None):
-    if 'return_to' in request.GET:
+def logout(request, return_to=None):
+    if return_to is None and 'return_to' in request.GET:
         return_to = request.GET.getone('return_to')
         if not url_is_valid(return_to):
             return_to = None
